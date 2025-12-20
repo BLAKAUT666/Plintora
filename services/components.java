4cf6d3266a71c24c92bc7d75aa503fc8349535be
Blakault666 <xandisaque@gmail.com>
2025-12-20 00:16:19 -0300

    deixei mais limpo

--- services/components.java
+++ services/components.java
@@ -2,22 +2,17 @@ package services;
 import dominio.Config.Panel.loginButton;
 import dominio.Config.Panel.panel;
 import dominio.Config.Panel.text;
-
 import java.awt.GridLayout;
-
-import dominio.Config.Panel.inputName;
-import dominio.Config.Panel.inputPassword;
 import dominio.Config.Panel.themeAdd.add;
 import dominio.Config.window.menu;
+import dominio.Login.Login;
 
 public class components{
 	// componentes principais
 	private menu mainMenu = new menu();
 	private panel painel = new panel();
 	private text textMain = new text();
-	private inputName inputUserName = new inputName();
-	private loginButton loginButtonClick = new loginButton();
-	private inputPassword password = new inputPassword();
+    private Login login = new Login();
 
     public components(){
 		main();
@@ -25,12 +20,6 @@ public class components{
 
 	private void main(){
 		painel.setLayout(new GridLayout(0, 1));
-
-		// painel principal
-		painel.add(textMain);
-		painel.add(inputUserName);
-		painel.add(password);
-		painel.add(loginButtonClick);
 		// menu
 		mainMenu.add(painel);
 	}
