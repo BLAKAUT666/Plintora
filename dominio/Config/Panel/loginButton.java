d8a87315f045fde086bd876f034fb7b1b91aa188
Blakault666 <xandisaque@gmail.com>
2025-12-20 00:14:04 -0300

    adicionar o click

--- dominio/Config/Panel/loginButton.java
+++ dominio/Config/Panel/loginButton.java
@@ -2,16 +2,9 @@ package dominio.Config.Panel;
 import javax.swing.*;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.LayoutManager;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.File;
-import java.io.IOException;
 
 public class loginButton extends JButton{
-	private inputName name = new inputName();
 
 	public loginButton(){
 		// configuração basica
@@ -23,29 +16,6 @@ public class loginButton extends JButton{
 		setVisible(true);
 		setText("Entrar");
 		setFont(fontButton);
-		
-		clickButton();
     }
 
-
-	// click button
-	private void clickButton(){
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent event){
-				try {
-
-					File fileName = new File(name.getText() + ".txt");
-					boolean isCreate = fileName.createNewFile();
-
-					if(isCreate == true){
-						System.out.println("arquivo criado com sucesso");
-					}
-
-				} catch(IOException error){
-					error.printStackTrace();
-				}
-			}
-		});
-	}
 }
