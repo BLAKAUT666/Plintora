d0daf0bb6729cfd2d67b69fc2d29236cb4986409
Blakault666 <xandisaque@gmail.com>
2025-12-22 23:10:30 -0300

    adicionei uma pasta aos inputs

---
+++ dominio/Login/inputs/userName.java
@@ -0,0 +1,24 @@
+package dominio.Login.inputs;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextField;
+
+public class userName extends JTextField {
+
+    private Color background = new Color(255, 255, 255);
+    private Color textColor = new Color(0, 0, 0);
+    private Font font = new Font("Inter", 0, 30);
+
+    public userName() {
+        setVisible(true);
+        setSize(100, 40);
+        setBackground(background);
+        setForeground(textColor);
+        setFont(font);
+    }
+
+    public String getName() {
+        return getText();
+    }
+}
