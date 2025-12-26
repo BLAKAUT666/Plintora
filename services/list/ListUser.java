d2a08e1805526c8e1082542ca362b2fc746ff219
Blakault666 <xandisaque@gmail.com>
2025-12-25 23:03:40 -0300

    adicionei nome e senha no JSON

---
+++ services/list/ListUser.java
@@ -0,0 +1,28 @@
+package services.list;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import dominio.Config.user;
+
+public class ListUser {
+    public void saveUser(user usuario){
+        String name = usuario.getNome();
+        String password = usuario.getPassword();
+
+        String json = "[\n" +
+                            "    {\n" +
+                                "    \"username\":  \"" + name + "\", \n" +
+                                "    \"password\": \"" + password + "\" \n" +
+                            "    }\n" +
+                        "]\n";
+
+        try(FileWriter writer = new FileWriter("user.json")){
+            writer.write(json);
+            System.out.println("Usuario salvo em JSON!");
+
+        } catch(IOException error){
+            error.printStackTrace();
+        }
+    }
+}
